b6c1e79f6376634bca109c7644a5e2c91da3b2b4
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-02-05 18:11:34 -0500

    Code to rebuild site.

--- code/SchedulingFunction/RebuildSite.cs
+++ code/SchedulingFunction/RebuildSite.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,9 +11,37 @@ namespace SchedulingFunction
     public static class RebuildSite
     {
         [FunctionName("rebuild-site")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo timer, ILogger log)
+        public static async Task RunAsync(
+            //[TimerTrigger("0/1 * * * * *")]TimerInfo timer,
+            [TimerTrigger("0 30 6 * * *")]TimerInfo timer,
+            ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            var githubToken = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+
+            using (var client = new HttpClient())
+            {
+                var media = new MediaTypeWithQualityHeaderValue(
+                    "application/vnd.github.mister-fantastic-preview");
+
+                client.DefaultRequestHeaders.Accept.Add(media);
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("token", githubToken);
+                client.DefaultRequestHeaders.UserAgent.Add(
+                    new ProductInfoHeaderValue(new ProductHeaderValue("vplauzon")));
+
+                var response = await client.PostAsync(
+                    "https://api.github.com/repos/vplauzon/vplauzon.github.io/pages/builds",
+                    new StringContent(string.Empty));
+                var content = await response.Content.ReadAsStringAsync();
+
+                log.LogInformation($"Response status: {response.StatusCode}");
+                log.LogInformation($"Response content: {content}");
+
+                if ((int)response.StatusCode >= 300)
+                {
+                    throw new InvalidOperationException("Failure to rebuild site");
+                }
+            }
         }
     }
 }
